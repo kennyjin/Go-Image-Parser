9f3520166ec543b164c57d0325afcb528b85e6c4
kennyjin <jianianjin@gmail.com>
2021-01-19 12:53:47 -0500

    Update parameters

--- README.md
+++ README.md
@@ -12,9 +12,17 @@ There are 2 types of Go board images to consider:
 
 It might be easy to identify all the positions of stones in a screenshot, but identifying stone positions from a real world Go board could be harder, as the stones could be improperly placed.
 
+### Usage
+
+Usage example (default parameters for Fox Go server):
+
+python image_parser.py 1.png out.sgf --default_len
+
+### Problems
+
 **Problem**: How to deal with the black/white square (indicating the next move) from Fox Go Server?
 
 **Problem**: How to deal with the triangle/circle indicating the current move?
 
-## Next Steps
+### Next Steps
 - [ ] Transform **screenshots** of the Go board into SGF files.
--- image_parser.py
+++ image_parser.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
+DEFAULT_LEN = [35, 265, 932, 2, 49]
+
 
 """
 Get the color of the stone on the specific square.
@@ -37,9 +39,10 @@ def get_game_position(go_board, edge_len_x, edge_len_y, stone_len, board_size=19
                              edge_len_x + stone_len * x:edge_len_x + stone_len * (x + 1)]
             pos_array[y][x] = get_stone_color(piece)
             # print(square_array[y])
-            # cv2.imshow('image', piece)
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows()
+            # if x == 18:
+            #     cv2.imshow('image', piece)
+            #     cv2.waitKey(0)
+            #     cv2.destroyAllWindows()
     return pos_array
 
 
@@ -135,20 +138,51 @@ def plot_histogram(square):
 # f.close()
 
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('image_path', help='Specify the path of the Go board image')
+parser.add_argument('sgf_path', help='Specify the path of the output SGF file')
+parser.add_argument('--default_len', help='if use the default arguments for lengths', action='store_true')
+parser.add_argument('-t', help='Specify edge top', type=int)
+parser.add_argument('-l', help='Specify edge left', type=int)
+parser.add_argument('-b', help='Specify board length', type=int)
+parser.add_argument('-e', help='Specify edge length', type=int)
+parser.add_argument('-s', help='Specify stone length', type=int)
+
+
+args = parser.parse_args()
+
+image_path = args.image_path
+sgf_path = args.sgf_path
+
+t, l, b, e, s = args.t, args.l, args.b, args.e, args.l
+
+if args.default_len:
+    (t, l, b, e, s) = tuple(DEFAULT_LEN)
+
+
+
 # Get sgf for fox Go server images
 # Still cannot identify current move yet
 # Still cannot identify the black/white squares from the mouse
-# img = cv2.imread('../images/12.PNG')
-img = cv2.imread('E:/Go-game-record/Fox_Go_Server/images/2019-06-10 (3).png')
+
+img = cv2.imread(image_path)
+
 # print(np.shape(img))
-board_img = get_go_board(img, 68, 531, 1865, 1865)
+
+# get_go_board(game_image, edge_top, edge_left, board_len_x, board_len_y)
+board_img = get_go_board(img, t, l, b, b)
+
 # cv2.imshow('image', board_img)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
-game_position = get_game_position(board_img, 3, 3, 98)
 
-# f = open("out3.sgf", "w")
-f = open("E:/Go-game-record/Fox_Go_Server/2019-06-10 (3).sgf", "w")
+# # get_game_position(go_board, edge_len_x, edge_len_y, stone_len, board_size=19):
+game_position = get_game_position(board_img, e, e, s)
+
+f = open(sgf_path, "w")
+
 f.write(get_sgf_txt(game_position))
 f.close()
 
